95dd48af11899d17d9017fbb81d1c76497e9b113
Benoit TRAVERS <btravers@insa-rennes.fr>
2015-03-11 12:43:40 +0100

    Blueflood interface

---
+++ src/plugins/blueflood/bluefloodCtrl.js
@@ -0,0 +1,59 @@
+define([
+	'angular',
+	'lodash',
+	'kbn'
+],
+function (angular, _, kbn) {
+	'use strict';
+
+	var module = angular.module('grafana.controllers');
+
+	module.controller('BluefloodCtrl', function($scope, $timeout) {
+
+		$scope.init = function() {
+			$scope.target.errors = validateTarget($scope.target);
+			$scope.aggregators = ['average', 'min', 'max', 'numPoints'];
+						
+			$scope.$on('typeahead-updated', function() {
+				$timeout($scope.targetBlur);
+			});
+		};
+
+		$scope.targetBlur = function() {
+			$scope.target.errors = validateTarget($scope.target);
+			// this does not work so good
+			if (!_.isEqual($scope.oldTarget, $scope.target) && _.isEmpty($scope.target.errors)) {
+				$scope.oldTarget = angular.copy($scope.target);
+				$scope.get_data();
+			}
+		};
+
+		$scope.duplicate = function() {
+			var clone = angular.copy($scope.target);
+			$scope.panel.targets.push(clone);
+		};
+
+		$scope.suggestMetrics = function(query, callback) {
+			$scope.datasource.performSuggestMetrics(query).then(callback);
+		};
+
+		$scope.suggestTargets = function(query, callback) {
+			$scope.datasource.performSuggestTargets(query).then(callback);
+		};
+
+		function validateTarget(target) {
+			var errs = {};
+
+			if (!target.metric) {
+				errs.metric = "You must supply a metric name.";
+			}
+
+			if (!target.tenant) {
+				errs.tenant = "You must supply a tenant name.";
+			}
+
+			return errs;
+		}		
+
+	});
+});
---
+++ src/plugins/blueflood/bluefloodDatasource.js
@@ -0,0 +1,126 @@
+define([
+	'angular',
+	'lodash',
+	'kbn',
+	'./bluefloodCtrl'
+],
+function (angular, _, kbn) {
+	'use strict';
+
+	var module = angular.module('grafana.services');
+
+	module.factory('BluefloodDatasource', function ($q, $http) {
+		
+		function BluefloodDatasource (datasource) {
+			this.type = 'blueflood';
+			this.name = datasource.name;
+			this.url = datasource.url;
+			
+			this.supportMetrics = true;
+			this.editorSrc = 'plugins/blueflood/editor.html';
+		}
+
+
+		/////////////////////////////////////////////////////////////////////////
+		/// Query
+		/////////////////////////////////////////////////////////////////////////
+
+		BluefloodDatasource.prototype.query = function (options) {
+			var from = convertToBluefloodTime(options.range.from);
+			var to = convertToBluefloodTime(options.range.to);
+			var targets = options.targets;
+			var select = options.agregators;
+
+			var promises = [];
+
+			angular.forEach(targets, function (target) {
+				if (!(!target.tenant || !target.metric || target.hide)) {
+					var options = {
+						method: 'GET',
+						url: '/v2.0/' + target.tenant + '/views/' + target.metric,
+						params: {
+							from: from,
+							to: to,
+							resolution: "FULL"
+						}
+					}
+					options.url = this.url + options.url;
+
+					if (agregators.length != 0) {
+						options.select = select.join();
+					}
+
+					promises.push($http(options).then(handleBluefloodQueryResponse));
+				}
+			}, this);
+
+			return $q.all(promises).then(function (responses) {
+				var series = [];
+				for (var i = 0; i < targets.length; i++) {
+					if (!(!target[i].tenant || !target[i].metric || target[i].hide)) {
+						var target = targets[i].tenant + ':' + targets[i].metric;
+						series.push({ target: target, datapoints: responses[i] });
+					}
+				}
+
+				return {
+					data: series
+				};
+			});
+		};
+
+
+		/////////////////////////////////////////////////////////////////////////
+		/// Handle responses
+		/////////////////////////////////////////////////////////////////////////
+
+		function handleBluefloodQueryResponse (result) {
+			if (!result) {
+				return [];
+			}
+
+			var datapoints = [];
+
+			angular.forEach(result.values, function (value) {
+				var t = value.timestamp;
+				var v = value.average;
+				datapoints.push([v, t]);
+			});
+
+			return datapoints;
+		}
+
+	
+		/////////////////////////////////////////////////////////////////////////
+		/// Suggest tenant and metric
+		/////////////////////////////////////////////////////////////////////////
+
+		GraphiteDatasource.prototype.performSuggestMetrics = function(query) {
+			return [];
+		};
+
+		GraphiteDatasource.prototype.performSuggestTenants = function(query) {
+			return [];
+		};
+
+
+		/////////////////////////////////////////////////////////////////////////
+		/// Time conversion functions specifics to Blueflood
+		/////////////////////////////////////////////////////////////////////////
+
+		function convertToBluefloodTime(date) {
+			if (date === 'now') {
+				return (new Date()).getTime();
+			}
+
+			date = kbn.parseDate(date);
+
+			return date.getTime();
+		}
+
+		/////////////////////////////////////////////////////////////////////////
+
+		return BluefloodDatasource;
+
+	});
+});
